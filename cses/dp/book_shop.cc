9fd1f66d7a9aa105a54c20f686c034e5d3b21b32
Tanishq Kumar <thetanishq@gmail.com>
2021-04-09 12:31:50 +0400

    bs push

---
+++ cses/dp/book_shop.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+#define p(j) cout << j << endl
+#define ll long long
+#define loop(i, b, n) for (int i = b; i < n; ++i)
+#define pb push_back
+#define desc greater<int>()
+#define vi vector<int>
+#define MI numeric_limits<int>::max()
+
+const int M = 1e9 + 1;
+
+template <typename T>
+vector<T> gl(int input_len);
+template <typename T>
+void pv(vector<T> vec);
+
+int main(){
+    // 0-1 knapsack dp
+}
+
+// helpers
+template <typename T>
+vector<T> gl(int n)
+{
+    vector<T> nums;
+    for (int i = 0; i < n; i++)
+    {
+        T el;
+        cin >> el;
+        nums.pb(el);
+    }
+    return nums;
+}
+
+template <typename T>
+void pv(vector<T> vec)
+{
+    for (int i = 0; i < vec.size(); ++i)
+        cout << vec[i] << " ";
+    cout << endl;
+}

e6e0819b5b8eb4c90050e4c761bff44c1242ebc2
Tanishq Kumar <thetanishq@gmail.com>
2021-07-11 16:37:52 +0400

    bs push

--- codeforces/div_b/368b.cc
+++ codeforces/div_b/368b.cc
@@ -14,21 +14,28 @@ using namespace std;
 #define loop(i, b, n) for (int i = b; i < n; ++i)
 #define pb push_back
 #define desc greater<int>()
+#define all(x) x.begin(), x.end()
 
 template <typename T>
 vector<T> gl(int input_len);
 template <typename T>
 void pv(vector<T> vec);
 
-int dp[101][101][11][11];
-
 int main(){
-    int n1, n2, k1, k2; 
-    cin >> n1 >> n2 >> k1 >> k2; 
-    // dp stores ways to permute soldiers given nh, nf, nch, ncf
-    
-
-
+    int n, m; cin >> n >> m; 
+    vi a = gl<int>(n);
+    vi suff(n); // suff[2] is how many distinct from a[2:]
+    vi count(1e5+1, 0);
+    suff[n-1] = 1; count[a[n-1]]++; 
+    for(int i = n-2; i >= 0; i--){
+        suff[i] = suff[i+1]; 
+        if (count[a[i]] == 0) suff[i]++;
+        count[a[i]]++; 
+    } 
+    while(m--){
+        int li; cin >> li; li--; 
+        p(suff[li]);
+    }
     return 0;
 }
 
